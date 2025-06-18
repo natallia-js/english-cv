4a789be259e8c208bc75343cda6a3a216a0f885d
Natallia Fedartsova <fedartsova@tut.by>
2025-06-18 19:12:12 +0300

    small changes to education

--- components/EducationBlock.js
+++ components/EducationBlock.js
@@ -12,16 +12,18 @@ export default class EducationBlock extends InfoBlock {
                 <div class="single-point-block">
                     <div class="info-item">
                         <div class="subheader">Belarussian State University of Computer Science and Radioengineering</div>
-                        Qualifications: System programmer, Mathematician, Master of engineering
+                        Qualifications: System programmer, Mathematician
                     </div>
-                    <div class="additional-info-item">Sep 2001 - Jun 2007</div>
+                    <!--<div class="additional-info-item">Sep 2001 - Jun 2007</div>-->
+                    <div class="additional-info-item">Master of engineering</div>
                 </div>
                 <div class="single-point-block">
                     <div class="info-item">
                         <div class="subheader">Institute of Mathematics, National Academy of Sciences</div>
                         PhD (physical and mathematical sciences)
                     </div>
-                    <div class="additional-info-item">Sep 2007 - Jan 2014</div>
+                    <!--<div class="additional-info-item">Sep 2007 - Jan 2014</div>-->
+                    <div class="additional-info-item">PhD</div>
                 </div>
             </div>
         `;
